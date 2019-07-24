c673c3e76f9267924d7aed4b7bd9be7c97cfa5db
Johannson Lim <30424050+mingyi850@users.noreply.github.com>
2019-07-24 17:35:13 +0800

    Machine Learning Files

---
+++ MachineLearning/src/azureStorage.py
@@ -0,0 +1,32 @@
+import requests
+from azure.storage.blob import BlockBlobService, PublicAccess, ContentSettings
+
+#Enter details of Azure Blob Storage Container Here
+storageAccountName = ""
+storageKey = ""
+storageContainer = ""
+
+
+dummyDict = {"imageURL": "None", "imagePath": "None"}
+
+def uploadBlob(filePath, blobName):
+                    
+    bbs = BlockBlobService(account_name=storageAccountName, account_key=storageKey)
+    bbs.create_blob_from_path(container_name=storageContainer,blob_name=blobName, file_path=filePath, content_settings=ContentSettings(content_type='image/jpg'))
+    imageURL = "https://{0}.blob.core.windows.net/{1}/{2}".format(storageAccountName, storageContainer, blobName)
+    imagePath = "{0}/{1}".format(storageContainer, blobName)
+    returnDict = {"imageURL": imageURL, "imagePath": imagePath}
+    return returnDict
+
+
+
+
+
+
+
+
+        
+        
+
+
+
